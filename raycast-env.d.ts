2b29f093dc27e280587a076bee75b2a1d9e86b45
ritualcasts <ritualiscozy@gmail.com>
2026-05-03 13:45:42 -0400

    Add files via upload

---
+++ raycast-env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `searchCards` command */
+  export type SearchCards = ExtensionPreferences & {}
+  /** Preferences accessible in the `viewRandomCard` command */
+  export type ViewRandomCard = ExtensionPreferences & {}
+  /** Preferences accessible in the `searchSets` command */
+  export type SearchSets = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `searchCards` command */
+  export type SearchCards = {}
+  /** Arguments passed to the `viewRandomCard` command */
+  export type ViewRandomCard = {}
+  /** Arguments passed to the `searchSets` command */
+  export type SearchSets = {}
+}
+
